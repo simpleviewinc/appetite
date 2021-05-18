f19dda2fbc6872740c4bac111938910714a0268c
mpcarolin <mpcarolin.dev@gmail.com>
2021-05-18 13:22:29 -0700

    removed module-alias

--- configs/jest.config.js
+++ configs/jest.config.js
@@ -4,10 +4,5 @@ require('dotenv').config()
 
 module.exports = {
   rootDir: path.resolve(__dirname, '../'),
-  testMatch: [ '<rootDir>/**/__tests__/**/*.js'],
-  moduleNameMapper: {
-    '^ATRoot$': '<rootDir>$1',
-    '^ATUtils$': '<rootDir>/src/utils$1',
-    '^ATApi$': '<rootDir>/src/api'
-  }
+  testMatch: [ '<rootDir>/**/__tests__/**/*.js']
 }
